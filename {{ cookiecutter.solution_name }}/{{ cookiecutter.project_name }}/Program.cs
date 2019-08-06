9974e5c5d87889088ee6c0a7b37a7e4ea3a974ab
Taylor Goodall <goodalltaylor92@gmail.com>
2019-08-06 18:55:00 +0930

    Add Sentry

--- {{ cookiecutter.solution_name }}/{{ cookiecutter.project_name }}/Program.cs
+++ {{ cookiecutter.solution_name }}/{{ cookiecutter.project_name }}/Program.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using System; {%- if cookiecutter.include_sentry %}
+using Sentry; {% endif %}
 
-namespace {{ cookiecutter.project_name }}
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace {{ cookiecutter.project_name }} {
+    class Program {
+        static void Main (string[] args) {
+
+            {%- if cookiecutter.include_sentry %}
+            using (SentrySdk.Init ("https://yoursentrydsn")) {
+
+            } {% endif %}
+
         }
     }
 }
